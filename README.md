5857106b16f67fbd5b23c9b22a53ad0295af3ed8
aereal <aereal@aereal.org>
2015-03-29 19:14:36 +0900

    minil new MouseX-Types-Data-Monad

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+MouseX::Types::Data::Monad - It's new $module
+
+# SYNOPSIS
+
+    use MouseX::Types::Data::Monad;
+
+# DESCRIPTION
+
+MouseX::Types::Data::Monad is ...
+
+# LICENSE
+
+Copyright (C) aereal.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+aereal <aereal@aereal.org>
---
+++ lib/MouseX/Types/Data/Monad.pm
@@ -0,0 +1,39 @@
+package MouseX::Types::Data::Monad;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+MouseX::Types::Data::Monad - It's new $module
+
+=head1 SYNOPSIS
+
+    use MouseX::Types::Data::Monad;
+
+=head1 DESCRIPTION
+
+MouseX::Types::Data::Monad is ...
+
+=head1 LICENSE
+
+Copyright (C) aereal.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+aereal E<lt>aereal@aereal.orgE<gt>
+
+=cut
+
