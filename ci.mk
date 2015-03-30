64af0cc715b4a686f95cffd52768f6e64bdbb6ab
aereal <aereal@aereal.org>
2015-03-30 13:07:14 +0900

    Define `coveralls` target

--- ci.mk
+++ ci.mk
@@ -2,6 +2,8 @@ CPANM = cpanm
 CARTON = carton
 CARTON_EXEC = $(CARTON) exec --
 PROVE = $(CARTON_EXEC) prove
+COVER = $(CARTON_EXEC) cover
+COVER_OPTIONS = -ignore_re 'inc|blib|modules|^t/|^scripts/|^local/' -make 'make -f ci.mk'
 TEST_TARGET = t/
 
 test:
@@ -12,3 +14,9 @@ prepare:
 
 deps:
 	$(CARTON) install
+
+coverage:
+	$(COVER) $(COVER_OPTIONS) -test
+
+coveralls: coverage
+	$(COVER) -report coveralls
